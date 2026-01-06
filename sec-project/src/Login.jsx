29dc389fb9a984ac24339598c764085f604b5d72
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-06 23:52:59 +0530

    Update Login.jsx

--- sec-project/src/Login.jsx
+++ sec-project/src/Login.jsx
@@ -1,10 +1,29 @@
-function Login(){
-    return(
-        <>
-            <p>LogIn</p>
-        </>
-        
-    )
+function Login() {
+  return (
+    <>
+      <form className = "my-5" style={{width:"50%",margin:"auto"}}>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input type="email" className="form-control"/>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" className="form-control"/>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Re-enter Password</label>
+          <input type="password" className="form-control"/>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input"/>
+          <label className="form-check-label">I Agree</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
 
-export  default Login
+export default Login;
