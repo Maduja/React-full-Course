cc0670c6bdeefb21bfec5ea038771f640447d306
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-08 12:09:06 +0530

    Update Login.jsx

--- sec-project/src/Login.jsx
+++ sec-project/src/Login.jsx
@@ -4,6 +4,7 @@ function Login() {
 
     const[pwd1, setPwd1] = useState("")
     const[pwd2, setPwd2] = useState("")
+    const[same,setSame] = useState("")
 
     function handlePwd1Change(event){
       setPwd1(event.target.value)
@@ -12,6 +13,15 @@ function Login() {
     function handlePwd2Change(event){
       setPwd2(event.target.value)
       console.log(event.target.value)
+    }
+    function handlePasswordCheck(){
+      if(pwd1==pwd2){
+        console.log("password matched")
+        setSame(true)
+      }else{
+        console.log("password not matched")
+        setSame(false)
+      }
     }
 
   return (
@@ -30,9 +40,12 @@ function Login() {
           <input type="password" onChange={handlePwd2Change} className="form-control" value={pwd2}/>
         </div>
         <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input"/>
+          <input type="checkbox" className="form-check-input" onChange={handlePasswordCheck}/>
           <label className="form-check-label">I Agree</label>
         </div>
+       {same && <p>Passwords Match</p>}
+       {!same && <p>Passwords not Match</p>}
+
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
