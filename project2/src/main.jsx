0787993216120286e7c5f6d890c3a0b82d21214a
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-10 07:38:56 +0530

    router

---
+++ project2/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Home from './Home.jsx'
+import Login from './Login.jsx'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home />
+  },
+  {
+    path:'login',
+    element:<Login />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
