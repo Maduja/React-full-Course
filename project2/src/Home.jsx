0854c15a4f00aa0cec01fb95f9f332849778eb54
Maduja Abhayasundara <117633491+Maduja@users.noreply.github.com>
2026-01-11 17:33:58 +0530

    Update Home.jsx

--- project2/src/Home.jsx
+++ project2/src/Home.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"
 import Login from "../../project2/src/Login"
 
+export const dataContext = useContext()
+
 function Home(){
+    const data = "datadata"
     
 return(
     <>
